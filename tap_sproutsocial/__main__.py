b5ade631a0fec166a89ff0100a9c0e0e1d812e96
Ella Wherritt <ella.wherritt@agilebits.com>
2024-09-25 12:13:58 -0600

    Initial commit

---
+++ tap_sproutsocial/__main__.py
@@ -0,0 +1,7 @@
+"""SproutSocial entry point."""
+
+from __future__ import annotations
+
+from tap_sproutsocial.tap import TapSproutSocial
+
+TapSproutSocial.cli()
---
+++ tap_sproutsocial/tap.py
@@ -0,0 +1,66 @@
+"""SproutSocial tap class."""
+
+from __future__ import annotations
+
+from singer_sdk import Tap
+from singer_sdk import typing as th  # JSON schema typing helpers
+
+# TODO: Import your custom stream types here:
+from tap_sproutsocial import streams
+
+
+class TapSproutSocial(Tap):
+    """SproutSocial tap class."""
+
+    name = "tap-sproutsocial"
+
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "auth_token",
+            th.StringType,
+            required=True,
+            secret=True,  # Flag config as protected.
+            description="The token to authenticate against the API service",
+        ),
+        th.Property(
+            "project_ids",
+            th.ArrayType(th.StringType),
+            required=True,
+            description="Project IDs to replicate",
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            description="The earliest record date to sync",
+        ),
+        th.Property(
+            "api_url",
+            th.StringType,
+            default="https://api.mysample.com",
+            description="The url for the API service",
+        ),
+        th.Property(
+            "user_agent",
+            th.StringType,
+            description=(
+                "A custom User-Agent header to send with each request. Default is "
+                "'<tap_name>/<tap_version>'"
+            ),
+        ),
+    ).to_dict()
+
+    def discover_streams(self) -> list[streams.SproutSocialStream]:
+        """Return a list of discovered streams.
+
+        Returns:
+            A list of discovered streams.
+        """
+        return [
+            streams.GroupsStream(self),
+            streams.UsersStream(self),
+        ]
+
+
+if __name__ == "__main__":
+    TapSproutSocial.cli()
